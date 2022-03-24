82fdff60ee33944052e4aa9131469f6c2d5219c5
TobeyKwon <tobeykwon@gmail.com>
2022-03-25 00:11:55 +0900

    0325 spring-mvc chapter4

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -1,10 +1,6 @@
 package hello.servlet.web.frontcontroller.v2;
 
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
-import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
-import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
-import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
 import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
 import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
@@ -18,26 +14,25 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/members/*")
 public class FrontControllerServletV2 extends HttpServlet {
-
-    private Map<String, ControllerV2> controllerV1Map = new HashMap<>();
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
 
     public FrontControllerServletV2() {
-        controllerV1Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerV1Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerV1Map.put("/front-controller/v2/members", new MemberListControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV2.service");
-        System.out.println(request.getRequestURI());
-        ControllerV2 controller = controllerV1Map.get(request.getRequestURI());
-        if (controller == null) {
+        ControllerV2 controller = controllerMap.get(request.getRequestURI());
+
+        if(controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
         }
+
         MyView myView = controller.process(request, response);
         myView.render(request, response);
     }
--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -5,29 +5,23 @@ import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MemberSaveControllerV2 implements ControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("MvcMemberSaveServlet.service");
-
+    public MyView process(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        //Model에 데이터를 보관한다.
+        String viewPath = "/WEB-INF/views/save.jsp";
         request.setAttribute("member", member);
-
-        return new MyView("/WEB-INF/views/save.jsp");
+        return new MyView(viewPath);
     }
 }
