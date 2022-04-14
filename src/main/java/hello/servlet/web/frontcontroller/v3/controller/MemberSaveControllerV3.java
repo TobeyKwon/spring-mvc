04645a70443425040f18db12b78cd4f97e054d48
TobeyKwon <tobeykwon@gmail.com>
2022-04-14 22:17:41 +0900

    0414 spring-mvc - 구조 이해

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+
+        Member member = new Member(paramMap.get("username"), Integer.parseInt(paramMap.get("age")));
+        memberRepository.save(member);
+
+        ModelView modelView = new ModelView("save-result");
+        modelView.getModel().put("member", member);
+        return modelView;
+    }
+}
